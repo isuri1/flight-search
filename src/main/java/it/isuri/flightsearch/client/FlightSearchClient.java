39757be07c7f57dd2a27e8002c3aff8ea85480b9
Isuri1 <isubabe1>
2021-03-05 15:34:45 +0800

    commit 1

--- src/main/java/it/isuri/flightsearch/client/FlightSearchClient.java
+++ src/main/java/it/isuri/flightsearch/client/FlightSearchClient.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//test
 @FeignClient(name = "flightSearchClient", url = "${flight.search.url}")
 public interface FlightSearchClient {
 
